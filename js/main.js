0150ef125c771a943ee66900a804ef946c878116
peterfrancella <peter.francella@outlook.com>
2015-10-18 23:39:01 -0400

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
-//	$(".gallery").addClass('active');
-	$('img:first').addClass('shown');
-	$('.name').text('Batman');
+$(document).ready(function(e){
+	
+	$('img:first').addClass('shown').next().addClass('hide');
+	$('.next').on('click', function(){
+		var $shown = $('.shown');
+		var title = $shown.removeClass('shown').next().addClass('shown').attr('title');
+			$('h1').text(title);
+	});
+	
+	
+	$('img:last').addClass('shown').next().addClass('hide');
+	$('.prev').on('click', function(){
+		var $shown = $('.shown');
+		var title = $shown.removeClass('shown').prev().addClass('shown').attr('title');
+			$('h1').text(title);
+	});
 
-});
-
-
-//	$('img:second').addClass('shown');
-//	$('.name').text('captain');
-
-//});
-
-$('.next').on('click', function(e){
-	$('next').addClass('shown');
-	$('.name').text('Captain America');
 
 });
